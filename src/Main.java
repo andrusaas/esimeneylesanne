784a57df1f4022603ab25fc49c586e2b626d27ea
prtjohanson <pjohanson@verticalscope.com>
2023-01-08 11:51:25 +0200

    Add some whitespace for readability.

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter temp:");
+
+	System.out.println("Enter temp:");
         double temp = in.nextDouble();
-        System.out.println("Enter units:(C or F)");
+
+	System.out.println("Enter units:(C or F)");
         String unit = in.next();
-        unit = unit.toUpperCase();
-        if (!(unit.equals("C") || unit.equals("F"))) {
+
+	unit = unit.toUpperCase();
+
+	if (!(unit.equals("C") || unit.equals("F"))) {
             System.out.println("Wrong unit!");
             return;
         }
-        Converter converter = new Converter();
-        DecimalFormat decimalFormat = new DecimalFormat("0.00");
-        if (unit.equals("C")) {
+
+	Converter converter = new Converter();
+
+	DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+
+	if (unit.equals("C")) {
             converter.setCelsius(temp);
             double f = converter.getFahrenheit();
             System.out.println("In Fahrenheit:" + decimalFormat.format(f));
         }
-        if (unit.equals("F")){
+
+	if (unit.equals("F")) {
             converter.setFahrenheit(temp);
             double c = converter.getCelsius();
             System.out.println("In Celsius:" + decimalFormat.format(c));
