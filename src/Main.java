fcd63433de25182410bfbd8631ea2d3393a95acb
andrusaas <andrus.aas@gmail.com>
2023-01-08 10:02:45 +0200

    Solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter temp:");
+        double temp = in.nextDouble();
+        System.out.println("Enter units:(C or F)");
+        String unit = in.next();
+        unit = unit.toUpperCase();
+        if (!(unit.equals("C") || unit.equals("F"))) {
+            System.out.println("Wrong unit!");
+            return;
+        }
+        Converter converter = new Converter();
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        if (unit.equals("C")) {
+            converter.setCelsius(temp);
+            double f = converter.getFahrenheit();
+            System.out.println("In Fahrenheit:" + decimalFormat.format(f));
+        }
+        if (unit.equals("F")){
+            converter.setFahrenheit(temp);
+            double c = converter.getCelsius();
+            System.out.println("In Celsius:" + decimalFormat.format(c));
+        }
     }
 }
